28602724bf28bdc8a7130bbef6d21e91566cd447
Hardik Chauhan <hardik11.chauhan@gmail.com>
2024-11-17 17:36:27 -0500

    Fix: Changed server url

--- packages/client/src/api/axiosConfig.ts
+++ packages/client/src/api/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3008",
+//   baseURL: "http://localhost:3008",
+  baseURL: "https://eduquest-j0w9.onrender.com",
 });
 
 export default axiosConfig;
